f04af19309d65e8a9f4371678eb05be0152218c2
Arttu <arttu@AM-C4R27ML6TL.local>
2022-08-03 22:20:37 +0300

    twitter share and share link

--- packages/frontend/components/Remix/RemixShareBox/index.tsx
+++ packages/frontend/components/Remix/RemixShareBox/index.tsx
@@ -11,11 +11,12 @@ export const RemixShareBox : React.FC<RemixShareBoxProps> = ({ publication }) =>
     const memeUrl = parseIpfs(publication.metadata.media[0].original.url)
 
     const handleShare = () => {
-
+        const url =  `https://memixer.xyz/meme/${publication.id}`
+        navigator.clipboard.writeText(url)
     }
 
     const handleShareTwitter = () => {
-
+        window.open(`https://twitter.com/intent/tweet?text=Just%20made%20a%20meme%20on%20Lens%20with%20Memixer:%20https://memixer.xyz/meme/${publication.id}`, "_blank")
     }
 
     const handleDownload = () => {
@@ -26,7 +27,7 @@ export const RemixShareBox : React.FC<RemixShareBoxProps> = ({ publication }) =>
         <div className="flex flex-col space-y-6 comic-border bg-white n:p-4 lg:p-10 rounded-4xl text-center">
             <a target="_blank" rel="noreferrer" href={`https://testnet.lenster.xyz/posts/${publication.id}`} className="comic-border-mini rounded-full bg-white px-10 py-2 cursor-pointer text-black no-underline">Collect/comment on Lenster</a>
             <div className="flex justify-center space-x-4 mb-4">
-                <div key={"sicon-share"} onClick={handleShare} className="rounded-full bg-white comic-border-mini flex items-center p-2 cursor-pointer">
+                <div key={"sicon-share"} onClick={() => handleShare()} className="rounded-full bg-white comic-border-mini flex items-center p-2 cursor-pointer">
                     <Image src="/assets/icons/share-icon.svg" width="30" height="30" alt="share" />
                 </div>
                 <div key={"sicon-twitter-share"} onClick={handleShareTwitter} className="rounded-full bg-white comic-border-mini flex items-center p-2 cursor-pointer">
