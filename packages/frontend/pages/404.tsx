b73ef7ce68df86d533dce30a28b1484bb210e9ef
Arttu <arttu@AM-C4R27ML6TL.local>
2022-08-09 14:50:52 +0300

    escape string

--- packages/frontend/pages/404.tsx
+++ packages/frontend/pages/404.tsx
@@ -6,7 +6,7 @@ const NotFound: React.FC<{}> = () => {
             <h2 className="text-2xl">404 • PAGE NOT FOUND</h2>
         </div>
         <div className="comic-border bg-white n:p-4 lg:p-10 rounded-4xl text-center mt-8 w-4/12">
-            <h2 className="text-base">Oops, we couldn't find the page you are looking for.</h2>
+            <h2 className="text-base">Oops, we couldn&apos;t find the page you are looking for.</h2>
             <button className="rounded-full border-black border-3 border-solid bg-purple px-6 py-2 font-bold mt-4 comic-border-mini rounded-full"><a className='no-underline text-black' href='/'>Go to Home</a></button>
         </div>
     </div>
