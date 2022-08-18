ae2684d0e2efa75a87313c2d5cb36b1f8da5fc56
Arttu <arttu@AM-C4R27ML6TL.local>
2022-08-18 11:28:32 +0300

    add github link to footer

--- packages/frontend/components/Footer/Footer/index.tsx
+++ packages/frontend/components/Footer/Footer/index.tsx
@@ -4,6 +4,7 @@ export const Footer = () => {
             <footer className="flex justify-between bottom-10 mt-5 mb-2 bg-blue w-10/12 rounded-full items-center p-6 ">
             <span className="text-sm text-white md:ml-10" >Memixer • Built by Newt Team @ AAVE</span>
             <div className="flex flex-wrap items-center text-sm">
+                <a className='no-underline text-white md:mr-16 sm:mr-10' href='https://github.com/WeAreNewt/Memixer'>GitHub</a>
                 <a className='no-underline text-white md:mr-16 sm:mr-10' href=''>Terms of use</a>
                 <a className='no-underline text-white' href=''>Privacy policy</a>
             </div >
