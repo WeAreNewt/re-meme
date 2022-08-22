09a4f6aa8c2828b3f5b01f4dae7d9f37f36a9c85
Joaquin Battilana <battilanajn@gmail.com>
2022-08-22 15:39:02 -0300

    environments

--- packages/frontend/components/Modals/ReportModal/index.tsx
+++ packages/frontend/components/Modals/ReportModal/index.tsx
@@ -31,9 +31,9 @@ export const ReportModal = ({ show, setShow, memeid } : ReportModalProps) => {
     return (
         <div onMouseDown={() => setShow(false)} className={`${show ? "block" : "hidden"} fixed h-screen w-screen z-20 flex items-center justify-center create-btn-gradient-transparent px-4 lg:px-0 top-0 left-0`}>
             <div onMouseDown={(e) => e.stopPropagation()} className="comic-border rounded-4xl bg-white n:p-4 lg:p-10">
-                <h3 className="mb-6">REPORT MEME</h3> 
+                <h3 className="text-subtitle-2">REPORT MEME</h3> 
                 
-                <h5>Why are you reporting this meme?</h5>
+                <h5 className="text-body-2-medium">Why are you reporting this meme?</h5>
                 <select
                 className="border-2 border-black border-solid rounded-xl w-full p-2 mb-2 mt-3 h-12"
                 //onChange={}
@@ -52,17 +52,17 @@ export const ReportModal = ({ show, setShow, memeid } : ReportModalProps) => {
                 <option value="Something else">Something else</option>
 
             </select>
-            <div className="mt-3">
-                <h6 className="whitespace-normal bg-[#FFE3B0] rounded-lg p-3">Your report is anonymous, except if you&apos;re reporting on intellectual property infringement.</h6>
+            <div className="mt-3 rounded-lg p-3 bg-[#FFE3B0]">
+                <h6 className="text-description-regular mb-0">Your report is anonymous, except if you&apos;re reporting on intellectual property infringement.</h6>
             </div>
             <div className="mb-6 mt-4">
-                <label className="mb-2 text-xl">Please, provide more information</label>
+                <label className="mb-2 text-body-2-medium">Please, provide more information</label>
                 <input type="text" placeholder="Type here" id="large-input" className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
             </div>
 
-                <div className="flex justify-end  mt-10">
-                    <button onClick={handleCancel} className={"flex items-center bg-white rounded-full p-3 border-black border-2 border-solid min-w-fit max-h-6 comic-border-mini mr-4"}> Cancel</button>
-                    <button onClick={onReport} className={"flex items-center bg-purple rounded-full p-3 border-black border-2 border-solid min-w-fit max-h-6 comic-border-mini"}>Report</button>
+                <div className="flex justify-end  mt-10 gap-2">
+                    <button onClick={handleCancel} className={"btn-medium-secondary"}> Cancel</button>
+                    <button onClick={onReport} className={"btn-medium"}>Report</button>
                 </div>
             </div>
         </div>
