451b8dd34a0bbed5f27b8a3f2443e1031ed99bef
Joaquin Battilana <battilanajn@gmail.com>
2022-08-31 14:29:33 -0300

    fixed footer

--- packages/frontend/components/Footer/Footer/index.tsx
+++ packages/frontend/components/Footer/Footer/index.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
-        <div className="flex justify-center items-center ">
-            <footer className="flex justify-between lg:fixed md:fixed sm:fixed bottom-8 mt-5 mb-2 bg-blue w-10/12 rounded-full items-center p-6 ">
+        <div className="flex justify-center items-center mt-auto mb-2">
+            <footer className="flex justify-between bottom-8 bg-blue w-10/12 rounded-full items-center p-6 ">
             <span className="text-sm text-white ml-10 sm:ml-5 md:mr-3" >re:meme • Built by Newt @ the Aave Fam</span>
             <div className="flex flex-wrap items-center text-sm">
                 <a className='no-underline text-white sm:mr-10 mr-3' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
