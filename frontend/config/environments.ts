cff1434c14a620047892dea0e187a8c90ffc2768
Joaquin Battilana <battilanajn@gmail.com>
2022-09-06 15:23:28 -0300

    changed module address

--- frontend/config/environments.ts
+++ frontend/config/environments.ts
@@ -14,7 +14,7 @@ export const environmentsConfig = {
         chain: chain.polygonMumbai,
         lensterUrl: 'https://testnet.lenster.xyz',
         appUrl: 'https://memixer.vercel.app/',
-        collectModuleAddress: '0xa8CEb9108D659c675145BEBdaB8CE37ee12E974D',
+        collectModuleAddress: '0xA78E4a4D0367f0f4674130F0Bb2653957ab5917e',
         lensHubAddress: '0x60Ae865ee4C725cd04353b5AAb364553f56ceF82'
     }
 }
