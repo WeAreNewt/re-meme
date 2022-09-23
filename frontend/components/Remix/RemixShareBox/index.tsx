af6f8a6bf9f652197aa7288ff704c019c90a8ebb
Arttu <arttu@AM-C4R27ML6TL.local>
2022-09-23 14:05:04 +0300

    add feedback to copy link button

--- frontend/components/Remix/RemixShareBox/index.tsx
+++ frontend/components/Remix/RemixShareBox/index.tsx
@@ -2,18 +2,26 @@ import { selectedEnvironment } from "../../../config/environments";
 import { PublicationData } from "../../../models/Publication/publication.model";
 import { parseIpfs } from "../../../utils/link";
 import FileSaver from 'file-saver'
+import { useState } from "react";
 
 type RemixShareBoxProps = {
     publication: PublicationData
 }
 
-export const RemixShareBox : React.FC<RemixShareBoxProps> = ({ publication }) => {
+export const RemixShareBox: React.FC<RemixShareBoxProps> = ({ publication }) => {
 
     const memeUrl = parseIpfs(publication.metadata.media[0].original.url)
 
+    const [imageHover, setImageHover] = useState(false)
+
+    const onImageHoverOut = () => {
+        setImageHover(false)
+    }
+
     const handleShare = () => {
-        const url =  `${selectedEnvironment.appUrl}/meme/${publication.id}`
+        const url = `${selectedEnvironment.appUrl}/meme/${publication.id}`
         navigator.clipboard.writeText(url)
+        setImageHover(true)
     }
 
     const handleShareTwitter = () => {
@@ -28,8 +36,11 @@ export const RemixShareBox : React.FC<RemixShareBoxProps> = ({ publication }) =>
         <div className="main-container">
             <a target="_blank" rel="noreferrer" href={`${selectedEnvironment.lensterUrl}/posts/${publication.id}`} className="btn-medium-secondary no-underline hover:text-neutral-black w-full mb-[16px]">Comment on Lenster</a>
             <div className="flex justify-center gap-[12px]">
-                <div key={"sicon-share"} onClick={handleShare} className="icon-btn-medium-secondary">
+                <div key={"sicon-share"} onClick={handleShare} onMouseOut={onImageHoverOut} className="icon-btn-medium-secondary">
                     <img className="icon-md" src="/assets/icons/share-icon.svg" alt="share" />
+                    {imageHover && <div className={`rounded-[31px] bg-black text-white items-center p-1 ${imageHover ? "!opacity-100" : "opacity-0"} absolute -top-11 `}>
+                        <span className="mr-4 ml-4">Copied!</span>
+                    </div>}
                 </div>
                 <div key={"sicon-twitter-share"} onClick={handleShareTwitter} className="icon-btn-medium-secondary">
                     <img className="icon-md" src="/assets/icons/share-twitter.svg" width="30" height="30" alt="share twitter" />
