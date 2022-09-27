6cbe06d2dc43b649bd0b251e91d78f270a3279c5
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-09-27 21:23:26 +0100

    changed the responsive nature of the footer

--- frontend/components/Footer/Footer/index.tsx
+++ frontend/components/Footer/Footer/index.tsx
@@ -1,14 +1,25 @@
 export const Footer = () => {
     return (
-        <div className="flex justify-center items-center mt-auto mb-2">
-            <footer className="flex justify-between bottom-8 bg-blue w-10/12 rounded-full items-center p-6 ">
-            <span className="text-sm text-white ml-10 sm:ml-5 md:mr-3" >re:meme • Built by Newt @ the Aave Fam</span>
-            <div className="flex flex-wrap items-center text-sm">
-                <a className='no-underline text-white sm:mr-10 mr-3' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
-                <a className='no-underline text-white sm:mr-10 mr-3' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a className='no-underline text-white sm:mr-10' href='terms-of-service'>Terms of Service</a>
-                <a className='no-underline text-white sm:mr-5 mr-10 ' href='privacy-policy'>Privacy Policy</a>
-            </div >
+        <div className="flex flex-col justify-center items-center mt-auto mb-2">
+            <footer className="flex flex-col w-10/12">
+                <div className="flex xl:hidden bg-blue rounded-full justify-center p-3">
+                    <span className="text-[12px] md:text-lg text-white ">re:meme • Built by Newt @ the Aave Fam</span>
+                </div>
+                <div className="bg-blue mt-[10px] flex flex-col items-center md:flex-row xl:hidden md:items-center md:justify-center rounded-3xl  p-3">
+                    <a className='no-underline text-white px-3 py-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
+                    <a className='no-underline text-white px-3 py-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a className='no-underline text-white px-3 py-2' href='terms-of-service'>Terms of Service</a>
+                    <a className='no-underline text-white px-3 py-2' href='privacy-policy'>Privacy Policy</a>
+                </div>
+                <div className="hidden text-md xl:flex xl:justify-between xl:items-center bg-blue xl:rounded-full p-6">
+                    <span className=" text-white" >re:meme • Built by Newt @ the Aave Fam</span>
+                    <div className="flex flex-wrap items-center ">
+                        <a className='no-underline text-white px-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
+                        <a className='no-underline text-white px-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
+                        <a className='no-underline text-white px-2' href='terms-of-service'>Terms of Service</a>
+                        <a className='no-underline text-white px-2' href='privacy-policy'>Privacy Policy</a>
+                    </div >
+                </div>
             </footer>
         </div>
     )
