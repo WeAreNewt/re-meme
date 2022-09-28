e53b2d2501c3d732f5e07a5f76121153c4e5dbe9
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-09-28 11:57:00 +0100

    fixed the margin bottom and the padding

--- frontend/components/Footer/Footer/index.tsx
+++ frontend/components/Footer/Footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div className="flex flex-col justify-center items-center mt-auto mb-2">
+        <div className="flex flex-col justify-center items-center mt-auto mb-[64px]">
             <footer className="flex flex-col w-10/12">
                 <div className="flex xl:hidden bg-blue rounded-full justify-center p-3">
                     <span className="text-[12px] md:text-lg text-white ">re:meme • Built by Newt @ the Aave Fam</span>
@@ -11,8 +11,8 @@ export const Footer = () => {
                     <a className='no-underline text-white px-3 py-2' href='terms-of-service'>Terms of Service</a>
                     <a className='no-underline text-white px-3 py-2' href='privacy-policy'>Privacy Policy</a>
                 </div>
-                <div className="hidden text-md xl:flex xl:justify-between xl:items-center bg-blue xl:rounded-full p-6">
-                    <span className=" text-white" >re:meme • Built by Newt @ the Aave Fam</span>
+                <div className="hidden text-md xl:flex xl:justify-between xl:items-center bg-blue xl:rounded-full  p-6">
+                    <span className=" text-white pl-2" >re:meme • Built by Newt @ the Aave Fam</span>
                     <div className="flex flex-wrap items-center ">
                         <a className='no-underline text-white px-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
                         <a className='no-underline text-white px-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
