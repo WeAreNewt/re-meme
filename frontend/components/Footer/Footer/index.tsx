040e7f0e5d30d47962afe785ec9a4f3d4475e104
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-09-28 12:03:50 +0100

    added links

--- frontend/components/Footer/Footer/index.tsx
+++ frontend/components/Footer/Footer/index.tsx
@@ -6,18 +6,18 @@ export const Footer = () => {
                     <span className="text-[12px] md:text-lg text-white ">re:meme • Built by Newt @ the Aave Fam</span>
                 </div>
                 <div className="bg-blue mt-[10px] flex flex-col items-center md:flex-row xl:hidden md:items-center md:justify-center rounded-3xl  p-3">
-                    <a className='no-underline text-white px-3 py-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
-                    <a className='no-underline text-white px-3 py-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
-                    <a className='no-underline text-white px-3 py-2' href='terms-of-service'>Terms of Service</a>
-                    <a className='no-underline text-white px-3 py-2' href='privacy-policy'>Privacy Policy</a>
+                    <a className='no-underline text-white px-4 py-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
+                    <a className='no-underline text-white px-4 py-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a className='no-underline text-white px-4 py-2' href='terms-of-service'>Terms of Service</a>
+                    <a className='no-underline text-white px-4 py-2' href='privacy-policy'>Privacy Policy</a>
                 </div>
                 <div className="hidden text-md xl:flex xl:justify-between xl:items-center bg-blue xl:rounded-full  p-6">
                     <span className=" text-white pl-2" >re:meme • Built by Newt @ the Aave Fam</span>
                     <div className="flex flex-wrap items-center ">
-                        <a className='no-underline text-white px-2' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
-                        <a className='no-underline text-white px-2' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
-                        <a className='no-underline text-white px-2' href='terms-of-service'>Terms of Service</a>
-                        <a className='no-underline text-white px-2' href='privacy-policy'>Privacy Policy</a>
+                        <a className='no-underline text-white pr-8' href='https://forms.office.com/r/MQi8tj5d7Z' target="_blank" rel="noopener noreferrer">Submit Appeal</a>
+                        <a className='no-underline text-white pr-8' href='https://github.com/WeAreNewt/re-meme' target="_blank" rel="noopener noreferrer">GitHub</a>
+                        <a className='no-underline text-white pr-8' href='terms-of-service'>Terms of Service</a>
+                        <a className='no-underline text-white pr-2' href='privacy-policy'>Privacy Policy</a>
                     </div >
                 </div>
             </footer>
